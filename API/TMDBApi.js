688c1d6d12a69a51a69e1c7f490c78052d87367d
Psary <4nthony.collin@gmail.com>
2019-03-10 16:18:57 +0100

    this commit is a lie

---
+++ API/TMDBApi.js
@@ -0,0 +1,13 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "7da6a5c429ae78be0cdee2daa7b60e1b";
+
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+  }
+  export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
